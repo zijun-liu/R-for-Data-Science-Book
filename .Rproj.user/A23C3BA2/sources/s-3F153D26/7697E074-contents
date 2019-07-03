ea0a3965a674bb5b8ea344ca2a40990c5d11d941
zijun-liu <zijunliu2014@gmail.com>
2019-07-03 15:48:49 -0700

    Initial commit

---
+++ .Rproj.user/A23C3BA2/sources/s-3F153D26/7697E074-contents
@@ -0,0 +1,2 @@
+print("This file is used to record the learning path of R")
+print("from book R for Data Science-O'Reilly")
---
+++ 3. Data trandformation with dplyr.R
@@ -0,0 +1,47 @@
+install.packages("tidyverse")
+install.packages(c("nycfligts13", "gapminder", "Lahman"))
+
+library(tidyverse)
+library(nycflights13)
+library(gapminder)
+library(Lahman)
+
+flights
+names(flights)
+
+##################### Grouped Summaries with summarize() #############################
+
+flights$dep_delay
+mean(flights$dep_delay, na.rm = T)
+
+summarize(flights, delay = mean(dep_delay, na.rm = T))
+
+# average delay per date
+by_day = group_by(flights, year, month, day)
+summarize(by_day, delay = mean(dep_delay, na.rm = T))
+
+##################### Combining multiple operations with Pipe ########################
+
+by_dest <- group_by(flights, dest)
+delay <- summarize(by_dest, 
+                   count = n(),
+                   dist = mean(distance, na.rm = T),
+                   delay = mean(arr_delay, na.rm = T))
+delay
+delay <- filter(delay, count > 20, dest != "HNL")
+delay
+
+ggplot(data = delay, mapping = aes(x = dist, y = delay)) +
+  geom_point(aes(size = count), alpha = 1/3) +
+  geom_smooth(se = FALSE)
+
+# frustrating code as we need to name each intermediate df, though we don't care
+# with pipe, %>%
+delays <- flights %>%
+  group_by(dest) %>%
+  summarize(count = n(),
+            dist = mean(distance, na.rm = T),
+            delay = mean(arr_delay, na.rm = T)
+            ) %>% 
+  filter(count > 20, dest != "HNL")
+
